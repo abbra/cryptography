cadc91115899848ecb842fabf02615f880011c7f
Alexander Bokovoy <abokovoy@redhat.com>
2026-02-01 06:38:42 +0200

    ML-DSA-87: add wycherproof vectors
    Signed-off-by: Alexander Bokovoy <abokovoy@redhat.com>

--- tests/wycheproof/test_mldsa.py
+++ tests/wycheproof/test_mldsa.py
@@ -16,6 +16,10 @@ from cryptography.hazmat.primitives.asymmetric.mldsa65 import (
     MlDsa65PrivateKey,
     MlDsa65PublicKey,
 )
+from cryptography.hazmat.primitives.asymmetric.mldsa87 import (
+    MlDsa87PrivateKey,
+    MlDsa87PublicKey,
+)
 
 from .utils import wycheproof_tests
 
@@ -87,6 +91,57 @@ def test_mldsa65_signature(backend, wycheproof):
         binascii.unhexlify(wycheproof.testgroup["privateSeed"])
     )
     public_key = MlDsa65PublicKey.from_public_bytes(
+        binascii.unhexlify(wycheproof.testgroup["publicKey"])
+    )
+
+    pkey_pkcs8 = wycheproof.testgroup.get("privateKeyPkcs8", None)
+    if pkey_pkcs8 is not None:
+        serialization.load_der_private_key(
+            binascii.unhexlify(pkey_pkcs8), None
+        )
+
+    testkey = private_key.public_key()
+
+    assert public_key.public_bytes(
+        serialization.Encoding.Raw, serialization.PublicFormat.Raw
+    ) == testkey.public_bytes(
+        serialization.Encoding.Raw, serialization.PublicFormat.Raw
+    )
+
+    msg = binascii.unhexlify(wycheproof.testcase["msg"])
+    expected_sig = binascii.unhexlify(wycheproof.testcase["sig"])
+    context = wycheproof.testcase.get("ctx", None)
+    if wycheproof.valid:
+        if context is not None:
+            context = binascii.unhexlify(context)
+            testkey.verify_with_context(expected_sig, msg, context)
+        else:
+            public_key.verify(expected_sig, msg)
+    else:
+        with pytest.raises(InvalidSignature):
+            if context is not None:
+                context = binascii.unhexlify(context)
+                testkey.verify_with_context(expected_sig, msg, context)
+            else:
+                public_key.verify(expected_sig, msg)
+
+
+@pytest.mark.supported(
+    only_if=lambda backend: backend.mldsa87_supported(),
+    skip_message="Requires OpenSSL with ML-DSA-87 support",
+)
+@wycheproof_tests("mldsa_87_sign_seed_test.json")
+def test_mldsa87_signature(backend, wycheproof):
+    if wycheproof.has_flag("Internal"):
+        alg = getattr(wycheproof.testfiledata, "algorithm", None)
+        pytest.skip(f"Internal implementation test for {alg}")
+
+    assert wycheproof.testgroup["type"] == "MlDsaSign"
+
+    private_key = MlDsa87PrivateKey.from_seed_bytes(
+        binascii.unhexlify(wycheproof.testgroup["privateSeed"])
+    )
+    public_key = MlDsa87PublicKey.from_public_bytes(
         binascii.unhexlify(wycheproof.testgroup["publicKey"])
     )
 
